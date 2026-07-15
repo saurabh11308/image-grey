dfc71e1456976354e6ef31bd4b1efebd928d551d
Saurabh <saurabh11308@yahoo.com>
2026-07-16 00:58:53 +0530

    First Commit

--- camera.py
+++ camera.py
@@ -4,6 +4,9 @@ from PIL import Image
 camera_img = st.camera_input(label="Take Photo")
 
 if camera_img:
+    #Create a Pillow image instance
     img = Image.open(camera_img)
+    #convert the pillow image instance to grey scale
     grey_img = img.convert('L')
+    #render the image to browser
     st.image(grey_img)
