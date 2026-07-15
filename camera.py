bad319ea3bdce91259b67ad806f9e0638799f255
Saurabh <saurabh11308@yahoo.com>
2026-07-16 00:56:34 +0530

    First Commit

--- camera.py
+++ camera.py
@@ -1,2 +1,9 @@
-import Streamlit as st
+import streamlit as st
 from PIL import Image
+
+camera_img = st.camera_input(label="Take Photo")
+
+if camera_img:
+    img = Image.open(camera_img)
+    grey_img = img.convert('L')
+    st.image(grey_img)
